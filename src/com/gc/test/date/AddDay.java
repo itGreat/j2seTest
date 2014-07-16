01002c88edd97866b1d89a5e8f701c991b320502
itGreat <itcoder@163.com>
2014-07-16 17:29:06 +0800

    s

--- src/com/gc/test/date/AddDay.java
+++ src/com/gc/test/date/AddDay.java
@@ -1,5 +1,7 @@
 package com.gc.test.date;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
@@ -18,11 +20,11 @@ public class AddDay {
 	}*/
 	
 	public static void main(String[] args) {
-		System.out.println("sssssss");
-		/*Calendar calendar=Calendar.getInstance();  
+		DateFormat fmt = new SimpleDateFormat("yyyy-MM-dd");
+		Calendar calendar=Calendar.getInstance();  
 		calendar.setTime(new Date());
-		System.out.println(calendar.get(Calendar.DAY_OF_MONTH));//今天的日期
-		calendar.set(Calendar.DAY_OF_MONTH,calendar.get(Calendar.DAY_OF_MONTH)+1);//让日期加1  
-		System.out.println(calendar.get(Calendar.DATE));//加1之后的日期Top 
-*/	}
+		System.out.println(fmt.format(calendar.getTime()));//今天的日期
+		calendar.set(Calendar.DAY_OF_MONTH,calendar.get(Calendar.DAY_OF_MONTH)+30);//让日期加1 
+		System.out.println(fmt.format(calendar.getTime()));//加1之后的日期Top 
+	}
 }
